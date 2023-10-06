9b936c0efbadf375b9817f581a502c9ef46a4808
MelvinMbae <melvinmuthomi95@gmail.com>
2023-10-06 23:57:47 +0300

    Final commit

--- house.py
+++ house.py
@@ -3,12 +3,11 @@
 name = input("What's your name? ")
 
 match name:
+    # Match name - what is attached to the match is what we are checking
+    # case: Value we are checking to determine action to take.
     case "Harry" | "Ron" | "Hermoine":
         print("Gryffindor")
     case "Draco":
         print("Slytherine")
     case _:
         print("Not sorted")
-
-
-        
