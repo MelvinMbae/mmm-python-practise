f50b7563c61392cbed5eb7eaceba9d7dcf5be03e
MelvinMbae <melvinmuthomi95@gmail.com>
2023-10-06 23:33:07 +0300

    match syntax

--- pairity.py
+++ pairity.py
@@ -1,1 +1,23 @@
+def main():
 
+    x = int(input("What is x? "))
+
+    if is_even(x):
+        print("Even")
+    else:
+        print("odd")
+
+
+def is_even(n):
+
+    # if n % 2 == 0:
+    #     return True
+    # else:
+    #     return False
+
+    return True if n % 2 == 0 else False
+
+# Or return n % 2 == 0 (No need to ask the question again
+# This is because the answer is either True or False hence no need for if else)
+
+main()
