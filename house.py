820a765c59c791513444e9287ec18ad0fdb6330c
MelvinMbae <melvinmuthomi95@gmail.com>
2023-10-06 23:52:12 +0300

    match learnt

---
+++ house.py
@@ -0,0 +1,13 @@
+# Similar to switch and case: This avoids writing too many elifs
+
+name = input("What's your name? ")
+
+match name:
+    case "Harry" | "Ron" | "Hermoine":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherine")
+    case _:
+        print("Not sorted")
+
+        
